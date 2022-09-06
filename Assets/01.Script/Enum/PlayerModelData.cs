674379d9a45d955fe6938d5f53e2273217825124
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-09-06 18:02:38 +0900

    0906_모델 4개 임포트, ChooseDatas SO로 변경중

--- Assets/01.Script/Enum/PlayerModelData.cs
+++ Assets/01.Script/Enum/PlayerModelData.cs
@@ -3,4 +3,9 @@
 public enum PlayerModelData
 {
     NONE,
+    UNITY_CHAN,
+    ALICE_CHAN,
+    KISORA_CHAN,
+    QUERY_CHAN,
+
 }
---
+++ Assets/01.Script/SO/ChooseDatas.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/ChooseDatas")]
+public class ChooseDatas : ScriptableObject
+{
+    [SerializeField]
+    private List<MusumeData> _playerDatas = new List<MusumeData>();
+    public List<MusumeData> playerDatas
+    {
+        get => _playerDatas;
+        set => _playerDatas = value;
+    }
+    [SerializeField]
+    private BossChooseOptionEnum _bossChooseOptionEnum = BossChooseOptionEnum.NONE;
+    public BossChooseOptionEnum bossChooseOptionEnum
+    {
+        get => _bossChooseOptionEnum;
+        set => _bossChooseOptionEnum = value;
+    }
+    [SerializeField]
+    private MapChooseOptionEnum _mapChooseOptionEnum = MapChooseOptionEnum.None;
+    public MapChooseOptionEnum mapChooseOptionEnum
+    {
+        get => _mapChooseOptionEnum;
+        set => _mapChooseOptionEnum = value;
+    }
+}
